03ed5ff38a280dac4375860e4c45cb7bbe674e45
Luciano Abbott <luciano.abbott@intelliware.com>
2021-05-26 15:17:30 -0400

    create ui lib with component xyz

---
+++ libs/ui/src/lib/xyz/xyz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mono-demo-xyz',
+  templateUrl: './xyz.component.html',
+  styleUrls: ['./xyz.component.scss']
+})
+export class XyzComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
